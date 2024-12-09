0edcd3cb4ac1ba85cb0c6dcd86f4f480a3271def
user <sydykovamuksina885@gmail.com>
2024-12-09 13:29:54 +0600

    homeworh2

--- js/home_works.js
+++ js/home_works.js
@@ -20,16 +20,88 @@
 
     //2
 
- const childBlock = document.querySelector(".child_block");
+// MOVE BLOCK
 
- let positionX = 0;
- let parentBlock = 450;
+const parentBlock = document.querySelector(".parent_block");
+const childBlock = document.querySelector(".child_block");
+const maxWidth = parentBlock.offsetWidth - childBlock.offsetWidth;
+const maxHeight = parentBlock.offsetHeight - childBlock.offsetHeight;
 
- const moveBlock = () => {
-     if (positionX < parentBlock) {
-         childBlock.style.left = `${positionX}px`
-         positionX++
-     }
-     requestAnimationFrame(moveBlock)
- }
- moveBlock()
+
+let positionX = 0;
+let positionY = 0;
+let direction = 1;
+
+
+const moveBlock = () => {
+    if(direction === 1){
+        if(positionX < maxWidth) {
+            childBlock.style.left = `${positionX}px`
+            positionX++
+        }else {
+            direction = 2;
+            positionX = maxWidth
+        }
+    }else if(direction === 2) {
+        if(positionY < maxHeight && positionX >= maxWidth) {
+            childBlock.style.top = `${positionY}px`
+            positionY++
+        }else {
+            direction = 3;
+        }
+    }else if(direction === 3){
+        if(positionX > 0 && positionY >= maxHeight){
+            childBlock.style.left = `${positionX}px`
+            positionX--
+        }else {
+            direction = 4
+        }
+    }else if(direction === 4) {
+        if(positionY > 0 && positionX >= 0) {
+            childBlock.style.top = `${positionY}px`
+            positionY--
+        }else {
+            direction = 1;
+            positionY = 0;
+        }
+    }
+
+    requestAnimationFrame(moveBlock)
+
+}
+moveBlock()
+
+/// Timer
+
+const seconds = document.querySelector('#seconds')
+
+let interval;
+let value = 0;
+let running = false ;
+
+const start = () => {
+    if(!running) {
+        interval = setInterval(() => {
+            value++
+            seconds.innerHTML = value
+        }, 1000)
+    }
+    running = true
+}
+
+
+const stop = () => {
+    clearInterval(interval)
+    running = false
+}
+const reset = () => {
+    clearInterval(interval)
+    value = 0
+    seconds.innerHTML = value
+    running = false
+}
+
+document.querySelector('#start').addEventListener('click', start)
+document.querySelector('#stop').addEventListener('click', stop)
+document.querySelector('#reset').addEventListener('click', reset)
+
