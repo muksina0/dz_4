97f6f997bb193a22a9c29f6827d371c60c9f249b
user <sydykovamuksina885@gmail.com>
2024-12-23 14:16:37 +0600

    homework6

--- js/lesson.js
+++ js/lesson.js
@@ -162,6 +162,11 @@ tabsParent.onclick = (event) => {
          updateCard()
      }
 
+ fetch('https://jsonplaceholder.typicode.com/posts')
+     .then( (response) => response.json())
+     .then((data) => console.log(data))
+
+
 
 
 
